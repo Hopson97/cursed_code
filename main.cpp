1778c5f2281f936d0e6d05c0ed118d79dc6fee9b
Hopson97 <mhopson@hotmail.co.uk>
2020-07-19 09:44:41 +0100

    Switch from tabs to spaces

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 
 #ifdef _WIN32
-#pragma warning( push )
-#pragma warning( disable : 4789 )
+    #pragma warning( push )
+    #pragma warning( disable : 4789 )
 #endif // _WIN32
 
 class CursedClass {
-	int
-		#include "letters"
-	;
+    int
+        #include "letters"
+    ;
 };
 
 template<typename T>
 void curse(const T* const cursed) {
 	const int* const cursed_pointer = reinterpret_cast<const int* const>(&cursed);
-	for (int i = 0; i < sizeof T / sizeof(int); i++) {
-		*const_cast<int*>(&cursed_pointer[i]) = i;
-	}
+    for (int i = 0; i < sizeof T / sizeof(int); i++) {
+        *const_cast<int*>(&cursed_pointer[i]) = i;
+    }
 }
 
 int main() {
-	char* cursed_memory = static_cast<char*>(std::calloc(1, sizeof CursedClass));
-	curse(reinterpret_cast<CursedClass*>(cursed_memory));
-	CursedClass cursed = *reinterpret_cast<CursedClass*>(cursed_memory);
-	int* cursed_pointer = reinterpret_cast<int*>(&cursed);
-	for (int i = 0; i < sizeof CursedClass / sizeof(int); i++) {
-		std::cout << *cursed_pointer + i << std::endl;
-	}
-	std::free(cursed_memory);
+    char* cursed_memory = static_cast<char*>(std::calloc(1, sizeof CursedClass));
+    curse(reinterpret_cast<CursedClass*>(cursed_memory));
+    CursedClass cursed = *reinterpret_cast<CursedClass*>(cursed_memory);
+    int* cursed_pointer = reinterpret_cast<int*>(&cursed);
+    for (int i = 0; i < sizeof CursedClass / sizeof(int); i++) {
+        std::cout << *cursed_pointer + i << std::endl;
+    }
+    std::free(cursed_memory);
 }
