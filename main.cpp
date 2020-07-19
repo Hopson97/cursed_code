fd8ea269606cfe0c1ed1e25c5a4bcf772846dcfa
Hopson97 <mhopson@hotmail.co.uk>
2020-07-19 10:10:33 +0100

    Ensure all sizeof uses ()

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
-#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
 
 #ifdef _WIN32
     #pragma warning( push )
@@ -18,18 +19,18 @@ class CursedClass {
 template<typename T>
 void curse(const T* const cursed) {
     const cursed_int(32)* const cursed_pointer = reinterpret_cast<const cursed_int(32)* const>(&cursed);
-    for (int i = 0; i < sizeof T / sizeof(cursed_int(32)); i++) {
+    for (int i = 0; i < sizeof(T) / sizeof(cursed_int(32)); i++) {
         *const_cast<int*>(&cursed_pointer[i]) = i;
     }
 }
 
 int main() {
-    char* cursed_memory = static_cast<char*>(std::calloc(1, sizeof CursedClass));
+    char* cursed_memory = static_cast<char*>(std::calloc(1, sizeof(CursedClass)));
     curse(reinterpret_cast<CursedClass*>(cursed_memory));
     CursedClass cursed = *reinterpret_cast<CursedClass*>(cursed_memory);
     cursed_int(32)* cursed_pointer = reinterpret_cast<cursed_int(32)*>(&cursed);
-    for (int i = 0; i < sizeof CursedClass / sizeof(cursed_int(32)); i++) {
-        std::cout << *cursed_pointer + i << std::endl;
+    for (int i = 0; i < sizeof(CursedClass) / sizeof(cursed_int(32)); i++) {
+        std::printf("%d\n", *cursed_pointer + i);
     }
     std::free(cursed_memory);
 }
