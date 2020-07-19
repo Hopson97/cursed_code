c9b58771d634955b1a8cdcd5719b85376647ec83
Hopson97 <mhopson@hotmail.co.uk>
2020-07-19 09:41:04 +0100

    ensure const correctness

--- main.cpp
+++ main.cpp
@@ -12,17 +12,17 @@ class CursedClass {
 };
 
 template<typename T>
-void curse(T* cursed) {
-	int* cursed_pointer = reinterpret_cast<int*>(&cursed);
+void curse(const T* const cursed) {
+	const int* const cursed_pointer = reinterpret_cast<const int* const>(&cursed);
 	for (int i = 0; i < sizeof T / sizeof(int); i++) {
-		cursed_pointer[i] = i;
+		*const_cast<int*>(&cursed_pointer[i]) = i;
 	}
 }
 
 int main() {
 	char* cursed_memory = static_cast<char*>(std::calloc(1, sizeof CursedClass));
+	curse(reinterpret_cast<CursedClass*>(cursed_memory));
 	CursedClass cursed = *reinterpret_cast<CursedClass*>(cursed_memory);
-	curse(&cursed);
 	int* cursed_pointer = reinterpret_cast<int*>(&cursed);
 	for (int i = 0; i < sizeof CursedClass / sizeof(int); i++) {
 		std::cout << *cursed_pointer + i << std::endl;
