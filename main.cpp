892ca4c29e6bb73524218723fb26e3b3a2a25b39
Hopson97 <mhopson@hotmail.co.uk>
2020-07-19 10:00:44 +0100

    Ensure all code uses the fixed size types

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cstddef>
 
-#define cursed_int(n) std::int##n##_t
-
 #ifdef _WIN32
     #pragma warning( push )
     #pragma warning( disable : 4789 )
 #endif // _WIN32
+
+#define cursed_int(n) std::int##n##_t
+
 
 class CursedClass {
     int
@@ -16,7 +17,7 @@ class CursedClass {
 
 template<typename T>
 void curse(const T* const cursed) {
-    const int* const cursed_pointer = reinterpret_cast<const cursed_int(32)* const>(&cursed);
+    const cursed_int(32)* const cursed_pointer = reinterpret_cast<const cursed_int(32)* const>(&cursed);
     for (int i = 0; i < sizeof T / sizeof(cursed_int(32)); i++) {
         *const_cast<int*>(&cursed_pointer[i]) = i;
     }
